e37377d8f9682b689b1fdbd925912113acbafc20
Ely Hess <Ely.hess@me.com>
2021-02-14 11:58:27 -0700

    Add before_destroy action & spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,4 +12,26 @@ RSpec.describe Item, type: :model do
     it { should have_many :invoice_items }
     it { should have_many(:invoices).through(:invoice_items) }
   end
+
+  describe "instance methods" do
+	  describe '#remove_invoices' do
+      it 'deletes only the item if there are other items on the invoice' do
+        item = create(:item)
+        invoice = create(:invoice)
+        invoice_item = create(:invoice_item, item: item, invoice: invoice)
+        invoice_item2 = create(:invoice_item, invoice: invoice)
+
+        expect(item.remove_invoices).to eq([])
+        expect(Invoice.count).to eq(1)
+      end
+      it 'deletes invoice if it has no other items on the invoice' do
+        item = create(:item)
+        invoice = create(:invoice)
+        invoice_item = create(:invoice_item, item: item, invoice: invoice)
+
+        expect(item.remove_invoices).to eq([invoice])
+        expect(Invoice.count).to eq(0)
+      end
+    end
+  end
 end
