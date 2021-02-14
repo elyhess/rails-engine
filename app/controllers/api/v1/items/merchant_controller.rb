c5db6264a209f6b7f8ad96e7222fd3a4c4ee1582
Ely Hess <Ely.hess@me.com>
2021-02-14 12:02:03 -0700

    Add merchant items index

--- app/controllers/api/v1/items/merchant_controller.rb
+++ app/controllers/api/v1/items/merchant_controller.rb
@@ -1,6 +1,14 @@
-class Api::V1::Items::MerchantController < ApplicationController
-	def index
-		item = Item.find(params[:item_id])
-		render json: MerchantSerializer.new(item.merchant)
+module Api
+	module V1
+		module Items
+			class MerchantController < ApplicationController
+
+				def index
+					item = Item.find(params[:item_id])
+					render json: MerchantSerializer.new(item.merchant)
+				end
+
+			end
+		end
 	end
 end
