494ee03652f7ddd5608c7a91facb13bdd61e249a
Ely Hess <Ely.hess@me.com>
2021-02-14 12:02:31 -0700

    Add merchant items spec

---
+++ spec/requests/api/v1/merchants/index_request_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe 'Merchant Items Index' do
+	it 'can get merchant for an item' do
+		merchant = create(:merchant)
+		5.times do
+			create(:item, merchant: merchant)
+		end
+
+		get "/api/v1/merchants/#{merchant.id}/items"
+
+		expect(response).to be_successful
+		items = JSON.parse(response.body, symbolize_names: true)
+
+		item_ids = items[:data].map { |item| item[:id].to_i }
+
+		expect(items[:data].size).to eq(5)
+		expect(items[:data][0]).to have_key(:id)
+		expect(items[:data][0]).to have_key(:type)
+		expect(items[:data][0]).to have_key(:attributes)
+		expect(item_ids).to eq(Item.ids.first(5))
+	end
+
+	it 'must have valid merchant id' do
+		get "/api/v1/merchants/145141223/items"
+
+		expect(response.status).to eq(404)
+	end
+end
