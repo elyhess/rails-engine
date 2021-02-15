e033a7f23f41a12099c1bdfdc28dec0853136b03
Ely Hess <Ely.hess@me.com>
2021-02-15 10:31:46 -0700

    Add find_one search method

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,7 +2,7 @@ class Search < ApplicationRecord
 
 	def self.find_one(collection, name)
 		collection.where('LOWER(name) LIKE ?', "%#{name.downcase}%")
-		          .order(name: :asc)
+		          .order(:name)
 		          .first
 	end
 
