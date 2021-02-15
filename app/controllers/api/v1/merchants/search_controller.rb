70126cc057380227cbea4d479acc607a75b1bc2b
Ely Hess <Ely.hess@me.com>
2021-02-15 10:32:05 -0700

    Implement find_one method

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -3,6 +3,15 @@ module Api
 		module Merchants
 			class SearchController < ApplicationController
 
+				def index
+					merchant = Search.find_one(Merchant, params[:name]) if params[:name]
+					if merchant
+						render json: MerchantSerializer.new(merchant)
+					else
+						render json: { "data": {} }
+					end
+				end
+
 			end
 		end
 	end
