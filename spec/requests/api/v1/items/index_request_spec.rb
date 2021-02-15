fef5fa7a949dace0be75ebdb122b7b6f7911095d
Ely Hess <Ely.hess@me.com>
2021-02-15 10:30:45 -0700

    Add API describe block

--- spec/requests/api/v1/items/index_request_spec.rb
+++ spec/requests/api/v1/items/index_request_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
-describe 'Item Merchant Index' do
-	it 'can get an items merchant' do
-		item = create(:item)
-		get "/api/v1/items/#{item.id}/merchant"
+describe "Items Api" do
+	describe 'Item Merchant Index' do
+		it 'can get an items merchant' do
+			item = create(:item)
+			get "/api/v1/items/#{item.id}/merchant"
 
-		expect(response).to be_successful
-		merchant = JSON.parse(response.body, symbolize_names: true)
+			expect(response).to be_successful
+			merchant = JSON.parse(response.body, symbolize_names: true)
 
-		expect(merchant[:data][:id].to_i).to eq(item.merchant_id)
-	end
+			expect(merchant[:data][:id].to_i).to eq(item.merchant_id)
+		end
 
-	it 'must have valid item id' do
-		get "/api/v1/items/51441241/merchant"
+		it 'must have valid item id' do
+			get "/api/v1/items/51441241/merchant"
 
-		expect(response.status).to eq(404)
+			expect(response.status).to eq(404)
+		end
 	end
 end
