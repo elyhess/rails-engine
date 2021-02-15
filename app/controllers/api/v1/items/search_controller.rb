72a126e34381602df2b10474e72c4bcdf61ff958
Ely Hess <Ely.hess@me.com>
2021-02-15 15:54:03 -0700

    Add search index

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -2,6 +2,14 @@ module Api
 	module V1
 		module Items
 			class SearchController < ApplicationController
+				def index
+					search = SearchFacade.find_all(params, Item)
+					if params[:name] && (params[:min_price] || params[:max_price])
+						render json: { "error" => {} }, status: 404
+					else
+						render json: ItemSerializer.new(search)
+					end
+				end
 			end
 		end
 	end
