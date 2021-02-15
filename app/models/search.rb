f0f8e00100d6d99217945ce4b87b6b24c1bbba50
Ely Hess <Ely.hess@me.com>
2021-02-15 15:53:44 -0700

    Add find_all_price method

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,9 +1,15 @@
 class Search < ApplicationRecord
 
-	def self.find_one(collection, name)
+	def self.find_all_name(collection, name)
 		collection.where('LOWER(name) LIKE ?', "%#{name.downcase}%")
 		          .order(:name)
-		          .first
+	end
+
+	def self.find_all_price(collection, min, max)
+		min_price = min || 0
+		max_price = max || Float::INFINITY
+		collection.where('unit_price BETWEEN ? AND ?', min_price, max_price)
+		          .order(:unit_price)
 	end
 
 end
