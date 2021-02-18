15d83a23c4c0e22547cffddba04673d479b2ec83
Ely Hess <Ely.hess@me.com>
2021-02-17 20:15:59 -0700

    Add revenue controller & request spec

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -1,13 +1,22 @@
 module Api
 	module V1
-		module Revenue
-			class MerchantsController < ApplicationController
+		class RevenueController < ApplicationController
 
-				def index
-					require "pry"; binding.pry
+			def index
+				if params[:start].present? && params[:end].present?
+					all_merchant_revenue = RevenueFacade.by_date(revenue_params)
+					render json: RevenueSerializer.total_revenue(all_merchant_revenue)
+				else
+					render json: {error: {}}, status: 400
 				end
-
 			end
+
+			private
+
+			def revenue_params
+				params.permit(:start, :end)
+			end
+
 		end
 	end
 end
